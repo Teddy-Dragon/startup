42daad3abf0d728291313586e0597285237e5aa2
Teddy-Dragon <kb3wfa@gmail.com>
2024-11-07 16:06:47 -0600

    Made the Map route its own component

--- startupReact/src/App.jsx
+++ startupReact/src/App.jsx
@@ -2,10 +2,13 @@ import { useState } from 'react'
 import {BrowserRouter, createBrowserRouter, Link, NavLink, Route, Routes} from "react-router-dom";
 import './App.css'
 import Navigation from "./components/navigation.jsx";
+import "./components/navigation.css"
+import Login from "./components/Login.jsx";
+import Maps from "./components/Maps.jsx";
 
 
 const router = createBrowserRouter([
-  {path: '/', name: 'Home', element: <App/>},
+  {path: "/home", name: 'Home', element: <App/>},
   {path: "/login", name: 'Login', element: <Login/>},
   {path: "/maps", name: 'Maps',element: <Maps/>},
   {path: "/session", name: 'Session', element: <Session/>},
@@ -14,85 +17,14 @@ const router = createBrowserRouter([
 
 ])
 
-function Login(){
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [username, setUsername] = useState('')
 
-    function handleReturn(){
-      const user = document.getElementById('username').value;
-      setUsername(user);
-
-      const pass = document.getElementById('password').value;
-      setPassword(pass);
-      // if username and password is equivalent to database, login
-
-
-    }
-    function handleNew(){
-      const email = document.getElementById('email').value;
-      setEmail(email);
-      const username = document.getElementById('newUsername').value;
-      setUsername(username);
-
-      const pass = document.getElementById('newPassword').value;
-      const confirm = document.getElementById('confirmPassword').value;
-      if (pass === confirm) {
-        setPassword(pass);
-        //confirm the password and store new user in database
-      }
-
-
-    }
-
-return <>
-  <link rel="stylesheet" href="/components/LoginCSS.css" />
-<div className="information">
-  <h3 className="infotitle">Returning Player?</h3>
-  <div className = "returning">
-  <li>
-    <form onSubmit={handleReturn} className= "returning">
-      <label>Username</label>
-      <input type="text" id={'username'} required/>
-        <label> Password </label>
-        <input type="password" id={'password'} required/>
-        <button type="submit">Submit</button>
-    </form>
-
-  </li>
-  </div>
-  <br/>
-  <h3> New Player? </h3>
-  <div className="newplayer">
-  <li>
-    <form onSubmit={handleNew}>
-      <label>Username</label>
-      <input type="text" id={'newUsername'} required/>
-      <label>Password</label>
-      <input type="password" id={'newPassword'} required/>
-      <label>Confirm Password</label>
-      <input type="password" id={'confirmPassword'} required/>
-      <label>Email</label>
-      <input type="email" id={'email'} required/>
-      <button type="submit">Submit</button>
-
-    </form>
-  </li>
-  </div>
-
-</div>
-</>
-}
-
-function Maps() {
-  return <>
-    <h1> I did something </h1>
-  </>
-
-}
 
 function Session() {
 
+
+    return<>
+
+    </>
 }
 
 function Game(){
